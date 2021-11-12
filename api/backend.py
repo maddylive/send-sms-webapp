e62c04bcd7623ac40f1aeeac2b251f7a27c63c89
Madhav Jha <madhavjha@gmail.com>
2021-11-11 23:43:18 -1000

    Calling api to send SMS

--- api/backend.py
+++ api/backend.py
@@ -1,5 +1,16 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+from_phone_number = os.environ['FROM_PHONE_NUMBER'] 
+to_phone_number = os.environ['TO_PHONE_NUMBER'] 
+
+client = Client(account_sid, auth_token)
 
 class handler(BaseHTTPRequestHandler):
 
@@ -7,5 +18,10 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    message = client.messages.create(
+                              body='Hi there',
+                              from_=from_phone_number,
+                              to=to_phone_number
+                          )
+    self.wfile.write(message.sid.encode())
     return
